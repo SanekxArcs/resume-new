02b184cdc4327fb2f1b44a785a100fd0cb530cee
Oleksandr Dzisiak <56607007+SanekxArcs@users.noreply.github.com>
2023-09-10 14:03:04 +0200

    Update Rodo.tsx

--- src/components/main/Rodo.tsx
+++ src/components/main/Rodo.tsx
@@ -4,7 +4,8 @@ const Rodo = () => {
   return (
     <div className="container px-4 mx-auto my-10 select-all">
       <a href="https://nofluffjobs.com/pl/log/praca-w-it/cv/klauzula-cv-2023-aktualna-wersja-niezbedna-przy-rekrutacjach/#:~:text=jest%20gotowa%20formu%C5%82a.-,Wyra%C5%BCam%20zgod%C4%99%20na%20przetwarzanie%20moich%20danych%20osobowych%20dla%20potrzeb%20niezb%C4%99dnych%20do,swobodnego%20przep%C5%82ywu%20takich%20danych%20oraz%20uchylenia%20dyrektywy%2095/46/WE%20(RODO)).,-Mo%C5%BCliwa%20jest%20r%C3%B3wnie%C5%BC">
-      <p className="text-xs text-muted-foreground">
+<p className="text-xs pb-2 text-muted-foreground">I consent to the processing of my personal data as part of the recruitment process for the position I have applied for. I consent to the processing of my personal data contained in this application for the purpose of future recruitment by the company to which I have applied.</p>
+      <p className="text-xs hidden text-muted-foreground">
         {`Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb
         niezbędnych do realizacji procesu rekrutacji (zgodnie z ustawą z dnia 10
         maja 2018 roku o ochronie danych osobowych (Dz. Ustaw z 2018, poz. 1000)
