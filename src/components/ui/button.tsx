2cc151bc9cbe84e2fae6a91d381efa2d2b0b08f1
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-09-11 00:49:13 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -50,8 +50,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
+        title="menu"
+        aria-label="menu"
       />
-    )
+    );
   }
 )
 Button.displayName = "Button"
