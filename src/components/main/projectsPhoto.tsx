f407d00d5ae27ba127b21cc73fe3231ddfee215e
Oleksandr Dzisiak <sanek.arc@gmail.com>
2023-09-24 09:58:50 +0200

    Update projectsPhoto.tsx

--- src/components/main/projectsPhoto.tsx
+++ src/components/main/projectsPhoto.tsx
@@ -25,16 +25,16 @@ const ProjectsPhoto = () => {
       </p>
       <div className="flex flex-wrap justify-center gap-2 mx-auto ">
         <div className="grid grid-cols-1 gap-2 p-2 rounded-md place-content-center md:grid-cols-5 md:grid-rows-3 lg:p-2 bg-muted">
-          <img className={`${imgClass} ${aspectRatioStN}`} src={pic1} alt="Photo" />
-          <img className={`${imgClass} ${aspectRatioOtO} md:col-span-3`} src={pic5} alt="Photo" />
-          <img className={`${imgClass} ${aspectRatioStN}`} src={pic2} alt="Photo" />
-          <img className={`${imgClass} ${aspectRatioStN} md:col-span-2 md:row-span-2`} src={pic3} alt="Photo" />
-          <img className={`${imgClass} ${aspectRatioStN}`} src={pic4} alt="Photo" />
-          <img className={`${imgClass} ${aspectRatioStN}`} src={pic6} alt="Photo" />
-          <img className={`${imgClass} ${aspectRatioStN}`} src={pic7} alt="Photo" />
-          <img className={`${imgClass} ${aspectRatioStN}`} src={pic9} alt="Photo" />
-          <img className={`${imgClass} ${aspectRatioOtO} md:col-span-2`} src={pic8} alt="" />
-          <img className={`${imgClass} ${aspectRatioStN} md:hidden`} src={pic10} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioStN}`} src={pic1} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioOtO} md:col-span-3`} src={pic5} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioStN}`} src={pic2} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioStN} md:col-span-2 md:row-span-2`} src={pic3} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioStN}`} src={pic4} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioStN}`} src={pic6} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioStN}`} src={pic7} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioStN}`} src={pic9} alt="Photo" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioOtO} md:col-span-2`} src={pic8} alt="" />
+          <img loading="lazy" className={`${imgClass} ${aspectRatioStN} md:hidden`} src={pic10} alt="Photo" />
         </div>
       </div>
     </>
