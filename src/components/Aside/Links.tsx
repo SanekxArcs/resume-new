da1fcbea7629987a275bd5b8adb2b80910e039ac
Oleksandr Dzisiak <56607007+SanekxArcs@users.noreply.github.com>
2024-04-13 19:32:52 +0200

    Update Links.tsx
    fix

--- src/components/Aside/Links.tsx
+++ src/components/Aside/Links.tsx
@@ -10,21 +10,21 @@ const Links = () => {
 
   const linkData = [
     {
-     link: "https://www.instagram.com/_margomti_tkachuk_/",
-  name: "@_margomti_tkachuk_",
-  title: "Мій профіль в Instagram. Також можете знайти у Google.",
-  icon: <Instagram className="mr-2" />,
-},
-{
-  link: "https://t.me/marrgori",
-  name: "t.me/marrgori",
-  title: "Мій контакт у Telegram",
-  icon: <Send className="mr-2" />,
+      link: "https://www.instagram.com/_margomti_tkachuk_/",
+      name: "@_margomti_tkachuk_",
+      title: "Мій профіль в Instagram. Також можете знайти у Google.",
+      icon: <Instagram className="mr-2" />,
+    },
+    {
+      link: "https://t.me/marrgori",
+      name: "t.me/marrgori",
+      title: "Мій контакт у Telegram",
+      icon: <Send className="mr-2" />,
     },
     {
       link: "https://www.behance.net/margaritkachuk",
       name: "behance.net/margaritkachuk",
-      title: "My behance contact",
+      title: "My Behance contact",
       icon: <Aperture className="mr-2" />,
     },
   ];
@@ -50,7 +50,7 @@ const Links = () => {
       <div className="break-before-avoid">
         <h3
           className="flex items-center justify-start cursor-default select-none "
-          title="First links i can share with you you can also google me and find more about me"
+          title="Перші посилання, які я можу поділитися з вами, ви також можете мене знайти в Google та дізнатися більше про мене"
         >
           <Link className="mr-2" /> Посилання
         </h3>
